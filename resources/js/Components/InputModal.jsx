92da4fb6d21421d4a2cc6b1605babc32b34eb6ff
Daisuke-Tai <kabislime@gmail.com>
2023-11-29 05:20:58 +0900

    モーダルと送信実装

--- resources/js/Components/InputModal.jsx
+++ resources/js/Components/InputModal.jsx
@@ -1,13 +1,74 @@
-export default function InputModal({ show, setShow, children }) {
+import { useForm } from '@inertiajs/react';
+import { useEffect } from 'react';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+
+export default function InputModal(props) {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    story: '',
+  });
+
+  useEffect(() => {
+      return () => {
+          reset('story');
+      };
+  }, []);
+
   const closeModal = () => {
-    setShow(false);
+    props.setShow(false);
   };
-  if (show) {
+
+  const submit = (e) => {
+      e.preventDefault();
+      
+      switch(props.type){
+        case 1 : // ジャンル作成
+          post(route('kinds.create'));
+        case 2 : // ブック作成
+          post(route('books.create',{'kind_id': props.kind_id}));
+        case 3 : // リレー
+          post(route('books.relay', {'id': props.id}));
+      };  
+
+      closeModal();
+  };  
+
+  
+  if (props.show) {
     return (
-      <div id="overlay" className="fixed top-0 left-0 w-full h-full bg-purple-900/40 flex items-center justify-center" onClick={closeModal}>
-        <div id="content1" className="z-10 w-1/2 p-4 bg-blue-200" onClick={(e) => e.stopPropagation()}>
-          {children}
-          <button onClick={closeModal}>作成</button>
+       
+      <div id="overlay" className="fixed top-0 left-0 w-full h-full bg-violet-500/50 flex items-center justify-center" onClick={closeModal}>
+        <div id="content1" className="z-10 w-1/2 p-4 h-1/2 bg-blue-200" onClick={(e) => e.stopPropagation()}>
+          <form onSubmit={submit}>
+
+            <div>
+                <InputLabel htmlFor="story" value={props.children} />
+                
+                <label class="text-gray-700" for="story">
+                    <textarea 
+                      id="story"
+                      type="submit"
+                      name="story"
+                      value={data.story}
+                      class="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
+                      placeholder="Enter your comment" 
+                      rows="10" 
+                      cols="50"
+                      autoComplete="write story"
+                      onChange={(e) => setData('story', e.target.value)}
+                    >
+                    </textarea>
+                </label>
+
+                <InputError message={errors.story} className="mt-2" />
+            </div>
+
+            <PrimaryButton className="ml-4" disabled={processing}>
+              つくる
+            </PrimaryButton>
+
+          </form>
         </div>
       </div>
     );
--- resources/js/Pages/Books/Index.jsx
+++ resources/js/Pages/Books/Index.jsx
@@ -4,7 +4,9 @@ import { useState } from 'react';
 import InputModal from '@/Components/InputModal';
 
 export default function Index({ auth,kinds,current_kind_id,books }) {
-    const [show, setShow] = useState(false);
+    const [show1, setShow1] = useState(false);
+    const [show2, setShow2] = useState(false);
+    const [show3, setShow3] = useState(false);
     
     return (
 
@@ -16,16 +18,11 @@ export default function Index({ auth,kinds,current_kind_id,books }) {
             <div className="flex h-screen" hidden md:block>
               {/* サイドバー*/}
               <div className="hidden w-60 overflow-y-auto md:block flex-shrink-0">
-                <nav className="bg-blue-100 mt-2 pt-2">
-                  <div className="bg-blue-300 mt-2 mb-2 px-2">テーマ</div>
-                  <div className="bg-red text-black">
-                    <a href={ route('kinds.create') } className="mb-2 px-2 flex items-center space-x-4 rounded text-black bg-gradient-to-r from-green-300 to-cyan-200">
-                      フォルダを追加する
-                    </a>
-                  </div>
+                <nav className="bg-violet-100 mt-2 pt-2">
+                  <div className="bg-blue-300 mt-2 mb-2 px-2">ジャンル</div>
                   <div>
-                      <button onClick={() => setShow(true)}>Click</button>
-                      <InputModal show={show} setShow={setShow}>
+                      <button onClick={() => setShow1(true)} className="inline-block rounded border border-current px-2 text-sm font-medium text-indigo-500 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500">ジャンルを作成</button>
+                      <InputModal type={1} show={show1} setShow={setShow1}>
                         <p>Childrenを使っています。</p>
                       </InputModal>
                     </div>
@@ -35,7 +32,7 @@ export default function Index({ auth,kinds,current_kind_id,books }) {
                       // 選択箇所をアクティブ
                       
                         <a key={kind.id} href={ route('books.index', {'kind_id': kind.id}) } 
-                        className="flex items-left hover:bg-gray-300 focus:bg-gray-300 list-group-item { current_kind_id === kind.id ? 'active' : '' }">
+                        className="flex items-left hover:bg-violet-300 focus:bg-violet-300 { current_kind_id === kind.id ? 'bg-violet-400' : '' }">
                            <span class="mx-2">
                           { kind.name }
                           </span>
@@ -49,45 +46,41 @@ export default function Index({ auth,kinds,current_kind_id,books }) {
               </div>
               
               {/* 真ん中*/}
-              <div className="bg-yellow-100 flex-grow w-full h-full px-2 overflow-y-auto">
-                  
-                <div className="bg-white mt-2 pt-2">
+              <div className="bg-violet-200 flex-grow w-full h-full mt-2 px-2 overflow-y-auto">   
+                <div className="bg-white pt-2">
                     <div className="bg-blue-400 mt-2 mb-2 px-2">ストーリー</div>
-                    <div className="mb-2 px-2 flex">
-                        <div className="text-right">
-                        <a href={ route('books.create', {'kind_id': current_kind_id}) } className="mb-2 px-2 flex items-center space-x-4 bg-orange-300">
-                            ストーリーをはじめる
-                        </a>
+                    <div className="mb-2 px-2">
+                        <div className="items-left">
+                          <button onClick={() => setShow2(true)} className="inline-block rounded border border-current px-2 text-sm font-medium text-indigo-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500">ストーリーをはじめる</button>
+                          <InputModal type={2} show={show2} setShow={setShow2} kind_id={current_kind_id}/>
                         </div>
                     </div>
-                    <table className="table">
-                        <thead>
-                        <tr>
-                        <th>ストーリー</th>
-                        <th>状態</th>
-                        <th>期限</th>
-                        <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
+                    <div className="w-full">
+
+                        
                         {books.map((book) => (
-                            <tr key={book.id}>
-                            <td>{ book.story }</td>
-                            <td>
-                                <span className="label { book.status_class }">{ book.status_label }</span>
-                            </td>
-                            <td>{ book.formatted_due_date }</td>
-                            <td><a href={ route('books.relay', {'id': book.id}) }>
-                              リレー</a></td>
-                            </tr>
+                          <div key={book.id}  className="border border-inherit transition hover:bg-violet-300 focus:bg-violet-300">
+                            <table className="w-full ml-2">
+                              <tbody>
+                                <tr>
+                                  <td className="whitespace-pre-line">{ book.story }</td>
+                                </tr>
+                                <tr>  
+                                  <td> <button onClick={() => setShow3(true)} className="px-2  inline-block rounded border border-current text-sm font-medium text-indigo-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500">リレー</button>
+                                  <InputModal type={3} show={show3} setShow={setShow3} id={book.id}/>  
+                                  </td>
+                                </tr>
+                                </tbody>
+                              </table>
+                          </div>
                         ))}
-                        </tbody>
-                    </table>
+                        
+                    </div>
                 </div>
               </div>
               
               {/* 3つ目*/}
-              <div className="grow w-80 mt-2 pt-2 pl-2 space-y-2 bg-green-100 hidden md:block">
+              <div className="grow w-80 mt-2 pt-2 pl-2 space-y-2 bg-violet-300 hidden md:block">
                 <div className="bg-white mt-2 pt-2">
                   aa
                 </div>
